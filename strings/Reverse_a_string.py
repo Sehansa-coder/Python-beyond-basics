898b7c80dab96add468a3b17a214a161bd0a7060
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-01-13 21:28:51 +0530

    Add a program to reverse a string and display it

---
+++ strings/Reverse_a_string.py
@@ -0,0 +1,22 @@
+# Problem :
+# Write a function that reverse a string using slicing
+
+def reversing(word):
+    return word[::-1]
+
+# Get user input
+input_word=input("Enter word : ")
+
+# call the function and display output
+print(reversing(input_word))
+
+
+# sample output : 
+# Enter word : world
+# dlrow
+
+# Enter word : amazing
+# gnizama
+
+# Enter word : good morning
+# gninrom doog
