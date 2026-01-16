d1cb43f62bb71479a71c208ece8e681b11df638d
D. B. Sehansa/Python🐍 <sehansadewlini27@gmail.com>
2026-01-16 23:11:11 +0530

    Enhance comments for joining list of words
    Added comments to explain the use of join() function.

--- strings/strings_joining/join_a_list_of_words_into_a_sentence.py
+++ strings/strings_joining/join_a_list_of_words_into_a_sentence.py
@@ -1,9 +1,10 @@
-
+# We use the format of "seperator".join(list elemet) to make a list of items into a string.
+# Below is an example
 
 words=["Python","is","awesome"]
 print(words)
 # Output as a list
-
+# Therefore : 
 # To convert the elements insilde the list, use .join() function 
 # " " is added between every element in the list
 sentence=" ".join(words)
