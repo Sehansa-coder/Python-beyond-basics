d02b08c16a273441b49e3765624e3399f7fbdd5d
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-01-16 22:04:18 +0530

    Created joining folder

---
+++ strings/strings_joining/join_a_list_of_words_into_a_sentence.py
@@ -0,0 +1,13 @@
+
+
+words=["Python","is","awesome"]
+print(words)
+# Output as a list
+
+# To convert the elements insilde the list, use .join() function 
+# " " is added between every element in the list
+sentence=" ".join(words)
+print(sentence)
+
+# output :
+# Python is awesome
