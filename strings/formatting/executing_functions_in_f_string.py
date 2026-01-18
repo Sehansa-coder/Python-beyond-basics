8b8307a62bf0c9b5a28db1c8e0c81586431c7bf5
D. B. Sehansa/Python🐍 <sehansadewlini27@gmail.com>
2026-01-18 23:59:19 +0530

    Enhance code clarity with comments
    Added comments to clarify the functionality of the code.

--- strings/formatting/executing_functions_in_f_string.py
+++ strings/formatting/executing_functions_in_f_string.py
@@ -11,9 +11,13 @@
 # the length of the sentence
 
 def execute_functions_in_f_string(name,fruit,sentence):
+    # convert the letters in name to upper case
     print(f"Name in uppercase: {name.upper()}")
+    # convert the letters in the fruit to lower case
     print(f"fruit in lowercase: {fruit.lower()}")
+    # strip() function remove the unwanted spaces in the beginning and end of user input. Not in the middle of the spaces
     print(f"cleaned sentence: {sentence.strip()}")
+    # print the length of the sentence after removing extra unwanted spaces by strip() function
     print(f"sentence length: {len(sentence.strip())}")
 
 
@@ -24,5 +28,14 @@ sentence=input("Enter a sentence:")
 
 # call the function
 execute_functions_in_f_string(name,fruit,sentence)
+
+# sample output:
+# Enter name:Andrea
+# Enter favourite fruit: ApplE
+# Enter a sentence:    I love python
+# Name in uppercase: ANDREA
+# fruit in lowercase: apple
+# cleaned sentence: I love python
+# sentence length: 13
 
 
