c9a2712dec6075a840755b06cc2a8c661fbccd3b
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-01-19 21:59:45 +0530

    Add a program explaining old python formatting techniques

---
+++ strings/formatting/percent_formatting.py
@@ -0,0 +1,28 @@
+# Old-style string formatting using %
+
+name="Lily"
+age=20
+score=87.567
+
+print("Name: %s"% name)  # %s -> string
+print("Age: %d"% age)    # %d -> integert
+print("Score: %f"% score)  # %f -> float
+print("Score rounded off to 2 decimal places: %.2f"% score) # output= 87.57
+
+print("My name is %s and I am %d years old."%(name,age)) # output - My name is Lily and I am 20 years old.
+
+# same output using f-strings (modern approach
+
+print(f"My name is {name} and I am {age} years old.") # output -My name is Lily and I am 20 years old.
+print(f"score: {score:.2f}")   # output= 87.57
+
+
+# Final output:
+# Name: Lily
+# Age: 20
+# Score: 87.567000
+# Score rounded off to 2 decimal places: 87.57
+# My name is Lily and I am 20 years old.  -\
+#                                           |   same
+# My name is Lily and I am 20 years old.  -/   
+# score: 87.57
