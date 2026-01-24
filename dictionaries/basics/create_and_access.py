f57b3f2b6e19cf8b9358f540a5412a0fed016744
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-01-24 23:05:16 +0530

    Add program to create and access items in a dictionary

---
+++ dictionaries/basics/create_and_access.py
@@ -0,0 +1,20 @@
+
+# Dictionary items are ordered, changeable, and do not allow duplicates
+# Dictionary items are presented in key:value pairs
+# can be used by referencing key name
+
+# example:
+
+# declare the dictionary
+mydict={
+    "age":20,
+    "name":"Alfred",
+    "country":"Sri Lanka"
+}
+
+# Access values using keys
+print(mydict["age"])         # direct access
+print(mydict["name"])
+# output:
+# 20
+# Alfred
