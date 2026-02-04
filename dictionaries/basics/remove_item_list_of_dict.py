c7141b1d2e1b4eeb0ccc574dc5866c55da517981
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-02-04 20:39:03 +0530

    Add a program of how to remove an item from a list of dictionaries

---
+++ dictionaries/basics/remove_item_list_of_dict.py
@@ -0,0 +1,30 @@
+# purpose:
+# This program demonstrate how to remove an item from a list of dictionaries.
+
+# Example:
+
+students=[
+    {"name":"David","age":18,"country":"Norway"},
+    {"name":"Rebecca","age":19,"country":"Africa"},
+    {"name":"Thomas","age":18,"country":"America"}
+]
+
+# Remove student of age 18
+new_list=[]
+for stu in students:
+    if stu["age"]!=18:
+        new_list.append(stu)
+
+students=new_list
+
+print("After removing:")
+for i in students:
+    print(i)
+
+# sample output:
+
+# After removing:
+# {'name': 'Rebecca', 'age': 19, 'country': 'Africa'}
+
+# Since both of the other students are 18 years old, they are removed.
+
