b441327c265378304e7cff340b209f5cd1a450b7
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-02-18 23:55:15 +0530

    Add program to convert to uppercase

---
+++ comprehension/list_comprehension/convert_strings_to_uppercase.py
@@ -0,0 +1,10 @@
+# list of names
+names=["seha","john","maria","alex"]
+
+# convert each name to uppercase
+upper_case_names=[i.upper() for i in names]
+
+print(upper_case_names)
+
+# output:
+# ['SEHA', 'JOHN', 'MARIA', 'ALEX']
