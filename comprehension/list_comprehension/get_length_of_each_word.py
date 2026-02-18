6512ecdd6451ae266b1eb763662194512653bbe6
D.B.S.Dewlini <sehansadewlini27@gmail.com>
2026-02-18 23:59:13 +0530

    Add program to find the length of each word using list comprehension

---
+++ comprehension/list_comprehension/get_length_of_each_word.py
@@ -0,0 +1,10 @@
+# list of words
+words=["python","developer","code","logic"]
+
+# create a list containing the length of each word
+word_length=[len(word) for word in words]
+
+print(word_length)
+
+# output:
+# [6, 9, 4, 5]
